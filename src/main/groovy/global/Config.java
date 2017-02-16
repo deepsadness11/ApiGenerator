54c3ed96fb6b8651d576965b2495c701e497c8c6
钟朝旭Frank-Android开发 <a295756092@gmail.com>
2017-02-16 09:07:00 +0800

    提交config文件

--- src/main/groovy/global/Config.java
+++ src/main/groovy/global/Config.java
@@ -3,25 +3,42 @@ package global;
 /**
  * Created by Administrator on 2017/2/14 0014.
  */
-public interface Config {
+public class Config {
+
+    public static final String API_DATA_DEV_READER = "http://apidev.baobaobooks.net/docs/readerclub/api_data.js?v=1487067289562";
+    public static final String API_PROJ_DEV_READER = "http://apidev.baobaobooks.net/docs/readerclub/api_project.js?v=1487151780547";
+    public static final String API_PROJ_DEV_BOOKSHELF = "http://apidev.baobaobooks.net/docs/bookshelf/api_project.js?v=1487158608795";
+    public static final String API_DATA_DEV_BOOKSHELF = "http://apidev.baobaobooks.net/docs/bookshelf/api_data.js?v=1487158608795";
+    public static final String API_DATA_DEV_USER = "http://apidev.baobaobooks.net/docs/user/api_data.js?v=1487158747269";
+    public static final String API_PROJ_DEV_USER = "http://apidev.baobaobooks.net/docs/user/api_project.js?v=1487158747269";
+//    String API_DATA_DEV_USER = "http://apidev.baobaobooks.net/docs/user/api_project.js?v=1487158747269";
+//    String API_PROJ_DEV_USER = "http://apidev.baobaobooks.net/docs/user/api_project.js?v=1487158747269";
 
 
-
-    String API_CACHE_FILE_NAME = "apiJson";
+    public static String API_CACHE_FILE_NAME = "apiJson";
     /**
      * 是否从网上重新请求。网上请求会缓存到本地
      */
-    boolean FROM_NET = false;
+    public static boolean FROM_NET = true;
+    /**
+     * 通过请求后生成
+     */
+    public static String COMMON_FILE_NAME = "Common";
 
-    interface PACKAGE_NAME {
-        String REQUEST_PARAM = "";
-        String RESPONSE_PARAM = "";
-        String SERVICE = "";
+
+    public static class PACKAGE_NAME {
+        public static String TOTAL_PACKAGE = "com.base.basesdk.data";
+        public static String REQUEST = TOTAL_PACKAGE + ".param";
+        public static String RESPONSE = TOTAL_PACKAGE + ".response";
+        public static String SERVICE = TOTAL_PACKAGE + ".http.service";
+        public static String COMMON = TOTAL_PACKAGE + ".http.common";
     }
 
-    interface FILE_PATH {
-        String REQUEST_PATH = "d://ag//request";
-        String RESPONSE_PATH = "d://ag//response";
-        String SERVICE_PATH = "d://ag";
+    public static class FILE_PATH {
+        public static String TOTAL_PATH = "d://ag";
+        public static String REQUEST = TOTAL_PATH + "//request";
+        public static String RESPONSE = TOTAL_PATH + "/response";
+        public static String SERVICE = TOTAL_PATH + "//http";
+        public static String COMMON = TOTAL_PATH + "//http//common";
     }
 }
